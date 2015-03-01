8dfa69ff41f624dd537489aa772e3e2d19327d0f
Jason Dalton <jason.dalton@listhub.com>
2015-02-28 21:42:02 -0500

    Added basic example of using ListHub Tracking API within requirejs.

--- www/app/main.js
+++ www/app/main.js
@@ -8,5 +8,16 @@ define(function (require) {
     // full IDs, like:
     var print = require('print');
 
+    // Basic inclusion of ListHub JS Tracking (async with builtin shim)
+    (function(l,i,s,t,h,u,b){l['ListHubAnalyticsObject']=h;l[h]=l[h]||function(){
+       (l[h].q=l[h].q||[]).push(arguments)},l[h].d=1*new Date();u=i.createElement(s),
+       b=i.getElementsByTagName(s)[0];u.async=1;u.src=t;b.parentNode.insertBefore(u,b)
+     })(window,document,'script','//tracking.listhub.net/la.min.js','lh');
+
+    // Initialize ListHub client.  
+    lh('init', {provider:'M-1234', test:true}); 
+    
+    // Note - notional ListHub Tracking event call in messages.getHello()
+    
     print(messages.getHello());
 });
--- www/app/messages.js
+++ www/app/messages.js
@@ -1,6 +1,8 @@
 define(function () {
     return {
         getHello: function () {
+            // Submit Event
+            lh('submit', 'TEST_EVENT', 'mlsn1234');
             return 'Hello World';
         }
     };
